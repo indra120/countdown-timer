ff377b04df7f4f95d1adfdd0ffcb6817e8ed7696
indra120 <indrapranata120@gmail.com>
2023-05-07 11:23:17 +0800

    Create the countdown logic

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const event = { name: 'New Years Eve', date: '1 Jan 2024' }
+const eventData = { name: 'New Years Eve', date: '1 Jan 2024' }
+const countsEl = document.querySelectorAll('.count')
+const formatTime = (time) => (time < 10 ? `0${time}` : time)
 
-document.getElementById('eventName').innerHTML = event.name
+document.getElementById('eventName').innerHTML = eventData.name
+
+function countdown() {
+	const totalSeconds = (new Date(eventData.date) - new Date()) / 1000
+	const { floor } = Math
+
+	const countsData = [
+		floor(totalSeconds / 3600 / 24),
+		floor(totalSeconds / 3600) % 24,
+		floor(totalSeconds / 60) % 60,
+		floor(totalSeconds) % 60,
+	]
+
+	countsEl.forEach((el, i) => {
+		const count = countsData[i]
+		el.innerHTML = i === 0 ? count : formatTime(count)
+	})
+}
+
+countdown()
+
+setInterval(countdown, 1000)
